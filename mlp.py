f9d902f50d532a7fa8b6143247eb6bee89d2238e
guangxush <guangxush@163.com>
2018-05-27 22:11:25 +0800

    mlp9.63

--- mlp.py
+++ mlp.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import tensorflow as tf
+sess = tf.Session()
+saver = tf.train.Saver()
+
 
 #定义添加隐含层的函数
 def add_layer(inputs, in_size, out_size, keep_prob=1.0, activation_function=None):
@@ -37,7 +40,6 @@ train_step = tf.train.AdagradOptimizer(0.35).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))       # 高维度的
 acuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))    # 要用reduce_mean
 
-tf.global_variables_initializer().run()
 #cost_accum = []
 acc_prev = 0
 
@@ -57,8 +59,7 @@ print input_Y.shape
 print input_X.dtype
 print input_Y.dtype
 
-sess = tf.Session()
-saver = tf.train.Saver()
+
 sess.run(tf.global_variables_initializer())
 for i in range(1000):
     train_step.run({x:input_X, y_:input_Y, keep_prob:0.75})
