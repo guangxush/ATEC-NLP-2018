5ab13411660bd0ef2bb075833ad33ce7905c82bc
guangxush <guangxush@163.com>
2018-05-27 21:27:39 +0800

    mlp9.3

--- mlp.py
+++ mlp.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
 sess = tf.InteractiveSession()
 
 
