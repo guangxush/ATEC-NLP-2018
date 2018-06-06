4077d096a62c54922c1b827e852f51bb0b510b08
guangxush <guangxush@163.com>
2018-06-06 21:25:24 +0800

    lstm_simple

--- LSTM/sim_lstm.py
+++ LSTM/sim_lstm.py
@@ -1,4 +1,3 @@
-
 # -*- coding:utf-8 -*-
 ########################################
 ## import packages
@@ -7,6 +6,7 @@ import os
 import re
 import csv
 import codecs
+
 import jieba
 import numpy as np
 
@@ -29,7 +29,7 @@ sys.setdefaultencoding('utf-8')
 ########################################
 # set directories and parameters
 ########################################
-DATA_DIR = '../data/'
+DATA_DIR = '../dataset/'
 EMBEDDING_FILE = '../model/w2v/w2v.mod'
 TRAIN_DATA_FILE = DATA_DIR + 'mytrain_pair.csv'
 TEST_DATA_FILE = DATA_DIR + 'mytest_pair.csv'
@@ -59,6 +59,115 @@ load_tokenizer = False
 save_path = "../model/lstm"
 tokenizer_name = "tokenizer.pkl"
 embedding_matrix_path = "../model/lstm/embedding_matrix.npy"
+
+# ########################################
+# ## index word vectors
+# ########################################
+# print('Indexing word vectors')
+#
+# word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True)
+# print('Found %s word vectors of word2vec' % len(word2vec.vocab))
+
+########################################
+# process texts in datasets
+########################################
+print('Processing text dataset')
+
+
+def text_to_wordlist(text, remove_stopwords=False):
+    # Clean the text, with the option to remove stopwords.
+
+    # Convert words to lower case and split them
+    words = list(jieba.cut(text.strip(), cut_all=False))
+
+    # Optionally, remove stop words
+    if remove_stopwords:
+        pass
+    # Return a list of words
+    return " ".join(words)
+
+
+texts_1 = []
+texts_2 = []
+labels = []
+with codecs.open(TRAIN_DATA_FILE, encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    header = next(reader)
+    for values in reader:
+        texts_1.append(text_to_wordlist(values[3]))
+        texts_2.append(text_to_wordlist(values[4]))
+        labels.append(int(values[5]))
+print('Found %s texts in train.csv' % len(texts_1))
+
+test_texts_1 = []
+test_texts_2 = []
+test_ids = []
+with codecs.open(TEST_DATA_FILE, encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    header = next(reader)
+    for values in reader:
+        test_texts_1.append(text_to_wordlist(values[3]))
+        test_texts_2.append(text_to_wordlist(values[4]))
+        test_ids.append(values[0])
+print('Found %s texts in train.csv' % len(test_texts_1))
+
+# print texts_1
+
+'''
+this part is solve keras.preprocessing.text can not process unicode
+start here
+'''
+
+
+def text_to_word_sequence(text,
+                          filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+                          lower=True, split=" "):
+    if lower: text = text.lower()
+    if type(text) == unicode:
+        translate_table = {ord(c): ord(t) for c, t in zip(filters, split * len(filters))}
+    else:
+        translate_table = keras.maketrans(filters, split * len(filters))
+    text = text.translate(translate_table)
+    seq = text.split(split)
+    return [i for i in seq if i]
+
+
+keras.preprocessing.text.text_to_word_sequence = text_to_word_sequence
+'''
+end here
+'''
+
+if load_tokenizer:
+    print('Load tokenizer...')
+    tokenizer = cPickle.load(open(os.path.join(save_path, tokenizer_name), 'rb'))
+else:
+    print("Fit tokenizer...")
+    tokenizer = Tokenizer(num_words=MAX_NB_WORDS, lower=False)
+    tokenizer.fit_on_texts(texts_1 + texts_2 + test_texts_1 + test_texts_2)
+    if save:
+        print("Save tokenizer...")
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        cPickle.dump(tokenizer, open(os.path.join(save_path, tokenizer_name), "wb"))
+
+sequences_1 = tokenizer.texts_to_sequences(texts_1)
+sequences_2 = tokenizer.texts_to_sequences(texts_2)
+test_sequences_1 = tokenizer.texts_to_sequences(test_texts_1)
+test_sequences_2 = tokenizer.texts_to_sequences(test_texts_2)
+# print sequences_1
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens' % len(word_index))
+
+data_1 = pad_sequences(sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
+data_2 = pad_sequences(sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
+labels = np.array(labels)
+print('Shape of data tensor:', data_1.shape)
+print('Shape of label tensor:', labels.shape)
+
+test_data_1 = pad_sequences(test_sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
+test_data_2 = pad_sequences(test_sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
+test_ids = np.array(test_ids)
 
 ########################################
 # prepare embeddings
@@ -100,6 +209,7 @@ np.save(embedding_matrix_path, embedding_matrix)
 #     weight_val *= 0.472001959
 #     weight_val[labels_val == 0] = 1.309028344
 
+
 ########################################
 # define the model structure
 ########################################
@@ -128,8 +238,11 @@ def get_model():
     merged = BatchNormalization()(merged)
     preds = Dense(1, activation='sigmoid')(merged)
 
-    model = Model(inputs=[sequence_1_input, sequence_2_input], outputs=preds)
-    model.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['acc'])
+    model = Model(inputs=[sequence_1_input, sequence_2_input], \
+                  outputs=preds)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='nadam',
+                  metrics=['acc'])
     model.summary()
     return model
 
