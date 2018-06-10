9e2137f3e24f126d54ad96985e091b91393af2cd
vicya <wangxiaoyuncu@gmail.com>
2018-06-10 21:19:57 +0800

    fix

--- LSTM/sim_lstm.py
+++ LSTM/sim_lstm.py
@@ -19,23 +19,21 @@ from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import keras.preprocessing.text
-
 import sys
 import cPickle
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
+from util.dataset import sentence_to_index_array,create_dictionaries,load_all_sentence
 ########################################
 # set directories and parameters
 ########################################
-DATA_DIR = '../dataset/'
-EMBEDDING_FILE = '../model/w2v/w2v.mod'
+DATA_DIR = './dataset/'
+EMBEDDING_FILE = './models/w2v/w2v.mod'
 TRAIN_DATA_FILE = DATA_DIR + 'mytrain_pair.csv'
 TEST_DATA_FILE = DATA_DIR + 'mytest_pair.csv'
-MAX_SEQUENCE_LENGTH = 30
+MAX_SEQUENCE_LENGTH = 20
 MAX_NB_WORDS = 200000
-EMBEDDING_DIM = 100
+EMBEDDING_DIM = 256
 VALIDATION_SPLIT = 0.1
 
 # num_lstm = np.random.randint(175, 275)
@@ -51,14 +49,14 @@ rate_drop_dense = 0.15
 act = 'relu'
 re_weight = True  # whether to re-weight classes to fit the 17.5% share in test set
 
-STAMP = '../model/lstm/lstm_%d_%d_%.2f_%.2f' % (num_lstm, num_dense, rate_drop_lstm, \
+STAMP = './models/lstm/lstm_%d_%d_%.2f_%.2f' % (num_lstm, num_dense, rate_drop_lstm, \
                                                 rate_drop_dense)
 
 save = True
 load_tokenizer = False
-save_path = "../model/lstm"
+save_path = "./models/lstm"
 tokenizer_name = "tokenizer.pkl"
-embedding_matrix_path = "../model/lstm/embedding_matrix.npy"
+embedding_matrix_path = "./models/lstm/embedding_matrix.npy"
 
 ########################################
 # define the model structure
@@ -102,7 +100,7 @@ def get_model(nb_words, embedding_matrix):
 
 def train_model(data_1, data_2, labels):
     print(STAMP)
-    model = get_model()
+    model = get_model(n_symbols,embedding_weights)
     early_stopping = EarlyStopping(monitor='val_loss', patience=3)
     bst_model_path = STAMP + '.h5'
     model_checkpoint = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
@@ -114,7 +112,23 @@ def train_model(data_1, data_2, labels):
 
 
 if __name__ == '__main__':
-    train_model()
+    model = Word2Vec.load('./models/word2vec_wx')
+    index_dict, word_vectors= create_dictionaries(model)
+    new_dic = index_dict
+    print ("Setting up Arrays for Keras Embedding Layer...")
+    n_symbols = len(index_dict) + 1  # 索引数字的个数，因为有的词语索引为0，所以+1
+    embedding_weights = np.zeros((n_symbols, 256))  # 创建一个n_symbols * 100的0矩阵
+    for w, index in index_dict.items():  # 从索引为1的词语开始，用词向量填充矩阵
+        embedding_weights[index, :] = word_vectors[w]  # 词向量矩阵，第一行是0向量（没有索引为0的词语，未被填充）
+    train_dataset1,train_dataset2,labels = load_all_sentence('./data/inputadd.txt','2')
+    print('load data1 ' + str(len(train_dataset1)))
+    print('load data2 ' + str(len(train_dataset2)))
+    train_dataset1 = sentence_to_index_array(new_dic, train_dataset1,'2',MAX_SEQUENCE_LENGTH)
+    train_dataset2 = sentence_to_index_array(new_dic, train_dataset2,'2',MAX_SEQUENCE_LENGTH)
+    print('sen2array'+ str(len(train_dataset1)))
+    print("训练集1shape： " + str(train_dataset1.shape))
+    print("训练集2shape： " + str(train_dataset2.shape))
+    train_model(train_dataset1,train_dataset2,labels)
 
 
 # predicts = model.predict([test_data_1, test_data_2], batch_size=10, verbose=1)
