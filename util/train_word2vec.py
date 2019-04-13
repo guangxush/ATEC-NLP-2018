f0abd484ab23bb0f94fa1c62d730e2a87b9c547e
guangxush <guangxush@163.com>
2019-04-13 10:22:51 +0800

    train word2vec data

--- util/train_word2vec.py
+++ util/train_word2vec.py
@@ -1,22 +1,42 @@
 # -*- coding:utf-8 -*-
 
-import logging
 from gensim.models.word2vec import LineSentence, Word2Vec
 import sys
+import os
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 
-# raw_sentences = ["the quick brown fox jumps over the lazy dogs","yoyoyo you go home now to sleep"]
+def train_word2vec():
+    input_file = '../data/atec_nlp_sim_train.csv'
+    data_prepare(input_file)
+    # raw_sentences = ["the quick brown fox jumps over the lazy dogs","yoyoyo you go home now to sleep"]
+    sentences = LineSentence("../data/train_questions_with_evidence.txt")
+    model = Word2Vec(sentences, min_count=1, iter=1000, size=256)
+    model.train(sentences, total_examples=model.corpus_count, epochs=1000)
 
-sentences= LineSentence("../data/train_questions_with_evidence.txt")
+    model.save("../models/w2v_256.mod")
+    model_loaded = Word2Vec.load("../models/w2v_256.mod")
+    sim = model_loaded.wv.most_similar(positive=[u'花呗'])
+    for s in sim:
+        print s[0]
 
-model = Word2Vec(sentences ,min_count=1, iter=1000,size=256)
-model.train(sentences, total_examples=model.corpus_count, epochs=1000)
 
-model.save("../models/w2v_256.mod")
-model_loaded = Word2Vec.load("../models/w2v_256.mod")
+def data_prepare(input_file):
+    print "data prepare start....."
+    if os.path.exists('../data/train_questions_with_evidence.txt'):
+        return
+    fr = open(input_file, 'r')
+    fw = open('../data/train_questions_with_evidence.txt', 'w')
+    for line in fr:
+        line = line.strip('\n').split('\t')
+        fw.write(str(line[1]) + '\n')
+        fw.write(str(line[2]) + '\n')
+    fr.close()
+    fw.close()
+    print('data prepared!!!!!')
 
-sim = model_loaded.wv.most_similar(positive=[u'花呗'])
-for s in sim:
-    print s[0]
+
+if __name__ == '__main__':
+    train_word2vec()
